32ce4bdf25c80ad54f9277af410a36940466bec4
cliecy <guanjiezou@gmail.com>
2024-05-31 00:50:30 +0900

    fix

--- src/Lib/lib.ts
+++ src/Lib/lib.ts
@@ -1,15 +1,15 @@
-import {Post, User,gender, userclass,} from "./typeDefinition";
+import {Post, Reply, User,gender, userclass,} from "./typeDefinition";
 
 
-export function GetPostPage():Post[]{
-    var postId = 0;
+export function GetPostPage():Reply[]{
+    var shareid = 0;
 
-    return [    {title: "BCD", content: "SSSS", date: new Date("2013/03/24"),shareid: "000",authorid:"3"}, {
-        title: "EFG",
-        content: "SSSS",
-        date: new Date("2013/03/24"),
-        shareid:"1",
-        authorid:"2"
+    return [ {
+        date:new Date(),
+        content:"ssss",
+        shareid:"123",
+        floor:2323,
+        authorid:"sdadas"
     }]
 }
 
