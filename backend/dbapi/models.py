bfe22ef67892d83891f4d812754445508296c98a
Mitsuhiro-Usesugi <1792887657@qq.com>
2024-06-01 16:19:43 +0900

    register no problem

--- backend/dbapi/models.py
+++ backend/dbapi/models.py
@@ -35,11 +35,11 @@ class Reply(Base):
 class User(Base):
     __tablename__ = "User"
     UserId: Mapped[int] = mapped_column(Integer, primary_key=True, nullable=True, autoincrement=True)
-    UserClass: Mapped[int] = mapped_column(Boolean, nullable=False, default=False)
+    UserClass: Mapped[int] = mapped_column(Boolean, nullable=False, default=0)
     UserName: Mapped[str] = mapped_column(String(10000), nullable=False, unique=True)
     motto: Mapped[str] = mapped_column(String(10000))
     LastLogintime: Mapped[datetime] = mapped_column(DATETIME, nullable=False)
-    gender: Mapped[int] = mapped_column(Integer, nullable=False)
+    gender: Mapped[str] = mapped_column(String(10000), nullable=False)
     password: Mapped[str] = mapped_column(String(10000), nullable=False)
     numofShares: Mapped[int] = mapped_column(Integer, nullable=False)
 
